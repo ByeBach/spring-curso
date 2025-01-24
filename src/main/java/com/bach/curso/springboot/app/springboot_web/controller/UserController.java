047b1c690b3b9994884d31789f09b5a5b5ee0d8e
Bach <byebachgm@gmail.com>
2025-01-24 16:15:21 -0300

    method list en progreso

--- src/main/java/com/bach/curso/springboot/app/springboot_web/controller/UserController.java
+++ src/main/java/com/bach/curso/springboot/app/springboot_web/controller/UserController.java
@@ -1,7 +1,11 @@
 package com.bach.curso.springboot.app.springboot_web.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.bach.curso.springboot.app.springboot_web.models.User;
@@ -12,9 +16,18 @@ public class UserController {
     @GetMapping("/details")
     public String details(Model model){
         User user = new User("bye", "Bach");
+        user.setEmail("byebach@gmail.com");
         model.addAttribute("title", "Hola Spring Framework con Model");
         model.addAttribute("user", user);
         return "details";
     }
+    @GetMapping("/list")
+    public String list(ModelMap model){
+        List<User> users = new ArrayList<>();
+
+        model.addAttribute("users", users);
+        return "list";
+    }
+
 
 }
