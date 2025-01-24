05a1ba14a7fef17d275daa2677029a40ddcfbca8
ByeBach <byebachgm@gmail.com>
2025-01-24 07:55:38 -0300

    user rest controlled add

--- src/main/java/com/bach/curso/springboot/app/springboot_web/controller/UserController.java
+++ src/main/java/com/bach/curso/springboot/app/springboot_web/controller/UserController.java
@@ -1,13 +1,19 @@
 package com.bach.curso.springboot.app.springboot_web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.bach.curso.springboot.app.springboot_web.models.User;
 
 @Controller
 public class UserController {
 
     @GetMapping("/details")
-    public String details(){
+    public String details(Model model){
+        User user = new User("bye", "Bach");
+        model.addAttribute("title", "Hola Spring Framework con Model");
+        model.addAttribute("user", user);
         return "details";
     }
 
