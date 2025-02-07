c23b3cd921eea33504a833157cec12efc7871c6a
Bach <byebachgm@gmail.com>
2025-02-07 19:52:01 -0300

    requestparam

---
+++ src/main/java/com/bach/curso/springboot/app/springboot_web/controller/RequestParamsController.java
@@ -0,0 +1,10 @@
+package com.bach.curso.springboot.app.springboot_web.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/params")
+public class RequestParamsController {
+
+}
--- src/main/java/com/bach/curso/springboot/app/springboot_web/controller/UserController.java
+++ src/main/java/com/bach/curso/springboot/app/springboot_web/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.bach.curso.springboot.app.springboot_web.models.User;
 
@@ -26,14 +27,22 @@ public class UserController {
     @GetMapping("/list")
     public String list(ModelMap model){
         
-        List<User> users = Arrays.asList(
-            new User("bye", "bach", "byebachgm@gmail.com"),
-            new User("el", "pepe", "elpepe@gmail.com"),
-            new User("cosme", "fulanito"));
+        // List<User> users = Arrays.asList(
+        //     new User("bye", "bach", "byebachgm@gmail.com"),
+        //     new User("el", "pepe", "elpepe@gmail.com"),
+        //     new User("cosme", "fulanito"));
 
-        model.addAttribute("users", users);
+        // model.addAttribute("users", users);
         model.addAttribute("title", "Listado de usuarios");
         return "list";
+    }
+    //para reutilizar datos en varias vistas
+    @ModelAttribute("users")
+    public List<User> usersModel(){
+        return Arrays.asList(
+            new User("bye", "bach", "byebachgm@gmail.com"),
+            new User("el", "pepe", "elpepe@gmail.com"),
+            new User("cosme", "fulanito")); 
     }
     @GetMapping("/testTemplate")
     public String templatesTest(Model model) {
