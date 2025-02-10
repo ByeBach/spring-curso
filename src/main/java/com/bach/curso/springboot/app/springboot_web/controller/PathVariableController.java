e9d9808b06bbe72be24e6833c1501f887108c83f
Bach <byebachgm@gmail.com>
2025-02-10 17:56:04 -0300

    Agregar nuevos endpoints en PathVariableController y configurar propiedades en application.properties

--- src/main/java/com/bach/curso/springboot/app/springboot_web/controller/PathVariableController.java
+++ src/main/java/com/bach/curso/springboot/app/springboot_web/controller/PathVariableController.java
@@ -3,6 +3,7 @@ package com.bach.curso.springboot.app.springboot_web.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,18 @@ import com.bach.curso.springboot.app.springboot_web.models.dto.ParamDto;
 @RestController
 @RequestMapping("/api/var")
 public class PathVariableController {
+
+    @Value("${config.username}")
+    private String username;
+
+    @Value("${config.message}")
+    private String message;
+
+    @Value("${config.ListOfValues}")
+    private String[] listOfValues;
+
+    @Value("${config.code}")
+    private Integer code;
 
     @GetMapping("/baz/{message}")
     public ParamDto baz(@PathVariable String message) {
@@ -37,5 +50,14 @@ public class PathVariableController {
         user.setName(user.getName().toUpperCase());
 
         return user;
+    }
+    @GetMapping("/values")
+    public Map<String, Object> values(){
+        Map<String, Object> json = new HashMap<>();
+        json.put("username", username);
+        json.put("message", message);
+        json.put("listOfValues", listOfValues);
+        json.put("code", code);
+        return json;
     }
 }
--- src/main/java/com/bach/curso/springboot/app/springboot_web/controller/UserRestController.java
+++ src/main/java/com/bach/curso/springboot/app/springboot_web/controller/UserRestController.java
@@ -1,17 +1,12 @@
 package com.bach.curso.springboot.app.springboot_web.controller;
 
-
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.bach.curso.springboot.app.springboot_web.models.User;
 import com.bach.curso.springboot.app.springboot_web.models.dto.UserDto;
 
