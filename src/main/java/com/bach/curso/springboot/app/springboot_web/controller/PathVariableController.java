66fd4c34f888267be0ee020bdb4b6b7fa259ea4c
Bach <byebachgm@gmail.com>
2025-02-11 10:18:21 -0300

    Agregar nuevos campos y propiedades en PathVariableController y values.properties

--- src/main/java/com/bach/curso/springboot/app/springboot_web/controller/PathVariableController.java
+++ src/main/java/com/bach/curso/springboot/app/springboot_web/controller/PathVariableController.java
@@ -1,9 +1,12 @@
 package com.bach.curso.springboot.app.springboot_web.controller;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,10 +28,25 @@ public class PathVariableController {
     private String message;
 
     @Value("${config.ListOfValues}")
-    private String[] listOfValues;
+    private List<String> listOfValues;
 
     @Value("${config.code}")
     private Integer code;
+
+    @Value("#{'${config.ListOfValues}'.split(',')}")
+    private List<String> valueList;
+
+    @Value("#{${config.valuesMap}}")
+    private Map<String, Object> valuesMap;
+
+    @Value("#{${config.valuesMap}.product}")
+    private String product;
+
+    @Value("#{${config.valuesMap}.price}")
+    private String price;
+
+    @Autowired
+    private Environment environment;
 
     @GetMapping("/baz/{message}")
     public ParamDto baz(@PathVariable String message) {
@@ -56,8 +74,13 @@ public class PathVariableController {
         Map<String, Object> json = new HashMap<>();
         json.put("username", username);
         json.put("message", message);
+        json.put("message2", environment.getProperty("config.message"));
         json.put("listOfValues", listOfValues);
         json.put("code", code);
+        json.put("valueList", valueList);
+        json.put("valuesMap", valuesMap);
+        json.put("product", product);
+        json.put("price", price);
         return json;
     }
 }
